fdd308cda1885c4c54d8bd01e4ac238caf53d269
mirhossein <mostlycoding@gmail.com>
2022-11-29 00:21:31 +0330

    codewars solution

---
+++ 8 Kyu/99 Problems, #5: reversing a list.js
@@ -0,0 +1,9 @@
+// Write a function that reverses a list, without using any built-in reverse functions.
+
+// JS : Array#reverse has been modified.
+
+const rev = (xs) => {
+  const arr = [];
+  xs.map((v) => arr.unshift(v));
+  return arr;
+};
